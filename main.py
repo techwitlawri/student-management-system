78adc25c12d7c8d4c4d5c7eaf94351ed34c868cb
Mmachi Ezeh <mmachilawri@gmail.com>
2024-09-11 14:14:29 +0100

    added database

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel,\
     QWidget, QGridLayout, QLineEdit, QPushButton, QMainWindow,\
-    QTableWidget
+    QTableWidget,  QTableWidgetItem
 import sys
 from PyQt6.QtGui import QAction
 import sqlite3
@@ -25,15 +25,25 @@ class MainWindow(QMainWindow):
         self.table = QTableWidget()
         self.table.setColumnCount(4)
         self.table.setHorizontalHeaderLabels(("Id", "Name", "Course", "Mobile"))
+        self.table.verticalHeader().setVisible(False)
         self.setCentralWidget(self.table)
-
+        
 
     def load_data(self):
-        self.table
+        connection = sqlite3.connect("database.db")
+        result = connection.execute("SELECT * FROM students")
+        self.table.setRowCount(0)
+        for row_number, row_data in enumerate(result):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                print(row_data)
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+        connection.close()
 
 
 app= QApplication(sys.argv)
 age_calculator = MainWindow()
 age_calculator.show()
+age_calculator.load_data()
 sys.exit(app.exec())
     
